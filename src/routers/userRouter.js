7ad1d7d6c441fa761d61f3b506988f687ec0fdd4
jmh622 <mh.jung.dev@gmail.com>
2022-03-11 13:52:34 +0900

    router 오류 수정

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -6,7 +6,7 @@ const userRouter = Router();
 
 userRouter.get('/edit', edit);
 userRouter.get('/remove', remove);
-userRouter.get('/:id', see);
 userRouter.get('/logout', logout);
+userRouter.get('/:id', see);
 
 export default userRouter;
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 
-import { upload, edit, deleteVidio, see } from '../controllers/videoController';
+import { upload, edit, deleteVideo, see } from '../controllers/videoController';
 
 const videoRouter = Router();
 
 videoRouter.get('/upload', upload);
 videoRouter.get('/:id', see);
 videoRouter.get('/:id/edit', edit);
-videoRouter.get('/:id/delete', deleteVidio);
+videoRouter.get('/:id/delete', deleteVideo);
 
 export default videoRouter;
