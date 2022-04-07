0533f88a71cb8d0a3b34e24a27b61433911d6b10
jmh622 <mh.jung.dev@gmail.com>
2022-04-07 22:29:09 +0900

    flash messages

--- src/middlewares.js
+++ src/middlewares.js
@@ -17,7 +17,7 @@ export const protectorMiddleware = (req, res, next) => {
 
 export const publicOnlyMiddleware = (req, res, next) => {
   if (req.session.loggedIn) {
-    req.flash('error', 'Not authorized');
+    req.flash('error', 'Log in first.');
     return res.redirect('/');
   }
 
